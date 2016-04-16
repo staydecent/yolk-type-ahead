73b2b813fd3eb51f54ce10b4976258264683b587
Adrian Unger <dev@staydecent.ca>
2016-04-15 20:45:23 -0700

    WIP to figure out streams vs callbacks.

--- src/TypeAhead.jsx
+++ src/TypeAhead.jsx
@@ -1,3 +1,4 @@
+import check from 'check-arg-types'
 import {h, render} from 'yolk'
 import {BehaviorSubject} from 'rxjs/BehaviorSubject' 
 
@@ -10,7 +11,7 @@ import 'rxjs/add/operator/withLatestFrom'
 export default TypeAhead
 
 
-const DEFAULT_POS = 0
+const INITIAL_POS = 0
 const KEY = {
   UP: 38,
   DOWN: 40,
@@ -22,12 +23,21 @@ const KEY_VALUES = [KEY.UP, KEY.DOWN, KEY.ENTER, KEY.ESC]
 function TypeAhead({props, createEventHandler}) {
   props = setDefaultProps(props)
 
-  const handleKeyUp = createEventHandler(ev => ev.keyCode)
-  const handleInput = createEventHandler(ev => ev.target.value)
+  if (!props.search) {
+    throw new Error('You need to provide a search function')
+  }
+
+  // Setup our events and streams
+  // ----
+
+  const handleInput = createEventHandler(ev => ev.target.value)  
   const displayValue = new BehaviorSubject('')
+  handleInput.subscribe(displayValue) // displayValue === ev.target.value
+  handleInput
+    .filter(val => val && val.length >= props.threshold)
+    .subscribe(getSearchResults)
 
-  handleInput.subscribe(displayValue)
-
+  const handleKeyUp = createEventHandler(ev => ev.keyCode)
   handleKeyUp
     
     // One of our `KEYS` was pressed
@@ -40,53 +50,79 @@ function TypeAhead({props, createEventHandler}) {
     .filter(({val}) => val && val.length >= props.threshold)
     
     // Ok, do stuff depending on the `keyCode`
-    .subscribe(({keyCode, val}) => {
-      switch (keyCode) {
-        case KEY.UP:
-          if (props.currPos > 0) {
-            props.currPos--
-          }
-          break
-        case KEY.DOWN:
-          let max = props.suggestions.length - 1
-          if (props.currPos < max || props.currPos === DEFAULT_POS) {
-            props.currPos++
-          }
-          break
-        case KEY.ENTER:
-          console.debug('handleSelection', props.suggestions[props.currPos] || val)
-          break
-        case KEY.ESC:
-          props.showSuggestions = false
-      }
-    })
+    .subscribe(handleKeyCodes)
+
+  // Set any template data
+  // ----
 
   let classes = ['type-ahead-holder'];
   if (props.showSuggestions) {
     classes.push('type-ahead-suggestions-visible')
-  } 
+  }
+
+  const ListItem = (suggestion, index) => {
+    let classes = ['type-ahead-list-item']
+    if (index === props.currPos) {
+      classes.push('active')
+    }
+    return <li className={classes}>{suggestion}</li>
+  }
 
   return (
     <div className={classes}>
+      <h1>{props.title} ({props.suggestions.map(x => x.length)})</h1>
       <input 
         placeholder={props.placeholder} 
-        // value={displayValue}
+        value={displayValue}
         onInput={handleInput}
         onKeyUp={handleKeyUp}
         //ng-blur="props.onBlur()" 
         className="type-ahead-input" />
 
       <ul className="type-ahead-list">
+        {props.suggestions.map(ListItem)}
       </ul>
     </div> 
   )
 
+  function getSearchResults(val) {
+    props.search.next(val)
+    const sub = props.search
+      .filter(val => check.prototype.toType(val) === 'array')
+      .subscribe(results => {
+        props.currPos = INITIAL_POS
+        props.suggestions = results
+        props.showSuggestions = results.length && val && val.length >= props.threshold
+      })
+    sub.unsubscribe()
+  }
 
+  function handleKeyCodes({keyCode, val}) {
+    console.debug('handleKeyCodes', keyCode, val, props)
+    switch (keyCode) {
+      case KEY.UP:
+        if (props.currPos > 0) {
+          props.currPos--
+        }
+        break
+      case KEY.DOWN:
+        let max = props.suggestions.length - 1
+        if (props.currPos < max || props.currPos === INITIAL_POS) {
+          props.currPos++
+        }
+        break
+      case KEY.ENTER:
+        console.debug('handleSelection', props.suggestions[props.currPos] || val)
+        break
+      case KEY.ESC:
+        props.showSuggestions = false
+    }
+  }
 }
 
 function setDefaultProps(props) {
-  props.currPos = props.currPos || DEFAULT_POS
-  props.suggestions = props.suggestions || []
+  props.currPos = props.currPos || INITIAL_POS
+  props.suggestions = props.suggestions || new BehaviorSubject([])
   props.showSuggestions = props.showSuggestions || false
   props.threshold = props.threshold || 2
   props.limit = props.limit || Infinity
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,16 @@
 import {h, render} from 'yolk'
+import {BehaviorSubject} from 'rxjs/BehaviorSubject'
+import check from 'check-arg-types'
+
 import TypeAhead from './TypeAhead.jsx'
 
+
+const search = new BehaviorSubject('')
+search
+  .filter(val => check.prototype.toType(val) === 'string')
+  .subscribe(str => search.next(['array', 'of', 'string', 'results']))
+
 render(
-  <TypeAhead title="TypeAhead">Type ahead my good friend</TypeAhead>,
+  <TypeAhead title="TypeAhead" search={search}>Type ahead my good friend</TypeAhead>,
   document.getElementById('container')
 )
